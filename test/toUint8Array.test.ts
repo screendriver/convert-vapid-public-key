6ea1b14b3c56f35536d04fabd18ba725fed06f25
Christian Rackerseder <git@echooff.de>
2018-11-14 21:14:28 +0100

    ✅ Test toUint8Array

---
+++ test/toUint8Array.test.ts
@@ -0,0 +1,9 @@
+import test from 'tape';
+import atob from 'atob';
+import { toUint8Array } from '../src/toUint8Array';
+
+test('should work', t => {
+  t.plan(1);
+  const actual = toUint8Array('BIW2q45TM1wfJz9', atob);
+  t.equal(actual.toString(), '4,133,182,171,142,83,51,92,31,39,63');
+});
