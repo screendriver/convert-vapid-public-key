53c0a7bb958de1e95faf7b54f8fd3f7da6128bcf
Christian Rackerseder <git@echooff.de>
2018-11-15 08:01:38 +0100

    👓 Change unit test title

--- test/toUint8Array.test.ts
+++ test/toUint8Array.test.ts
@@ -2,7 +2,7 @@ import test from 'tape';
 import atob from 'atob';
 import { toUint8Array } from '../src/toUint8Array';
 
-test('should work', t => {
+test('convert given base64 string to an Uint8Array', t => {
   t.plan(1);
   const actual = toUint8Array('BIW2q45TM1wfJz9', atob);
   t.equal(actual.toString(), '4,133,182,171,142,83,51,92,31,39,63');
