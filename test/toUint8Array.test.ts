961db035627bf23074c8af480478d90f04122b80
Christian Rackerseder <git@echooff.de>
2019-08-29 23:23:08 +0200

    ✅ Use jest instead of tape

--- test/toUint8Array.test.ts
+++ test/toUint8Array.test.ts
@@ -1,9 +1,7 @@
-import test from 'tape';
 import atob from 'atob';
 import { toUint8Array } from '../src/toUint8Array';
 
-test('convert given base64 string to an Uint8Array', t => {
-  t.plan(1);
+test('convert given base64 string to an Uint8Array', () => {
   const actual = toUint8Array('BIW2q45TM1wfJz9', atob);
-  t.equal(actual.toString(), '4,133,182,171,142,83,51,92,31,39,63');
+  expect(actual.toString()).toBe('4,133,182,171,142,83,51,92,31,39,63');
 });
