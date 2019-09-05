ac002ce2f82928a0dc7a41334cad7fc8dad24410
Christian Rackerseder <git@echooff.de>
2019-09-05 08:12:05 +0200

    ♻️ Use AVA instead of Jest in all tests

--- test/toUint8Array.test.ts
+++ test/toUint8Array.test.ts
@@ -1,7 +1,8 @@
+import test from 'ava';
 import atob from 'atob';
 import { toUint8Array } from '../src/toUint8Array';
 
-test('convert given base64 string to an Uint8Array', () => {
+test('convert given base64 string to an Uint8Array', t => {
   const actual = toUint8Array('BIW2q45TM1wfJz9', atob);
-  expect(actual.toString()).toBe('4,133,182,171,142,83,51,92,31,39,63');
+  t.is(actual.toString(), '4,133,182,171,142,83,51,92,31,39,63');
 });
