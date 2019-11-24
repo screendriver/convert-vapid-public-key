f1e6d27a7205b822b6749210cf27e8cae4a7b364
Christian Rackerseder <git@echooff.de>
2019-11-24 21:02:14 +0100

    💄 Use Chai's TDD assert style

--- test/toUint8Array.test.ts
+++ test/toUint8Array.test.ts
@@ -1,10 +1,10 @@
-import { expect } from 'chai';
+import { assert } from 'chai';
 import atob from 'atob';
 import { toUint8Array } from '../src/toUint8Array';
 
 suite('toUint8Array', function() {
   test('convert given base64 string to an Uint8Array', function() {
     const actual = toUint8Array('BIW2q45TM1wfJz9', atob);
-    expect(actual.toString()).to.equal('4,133,182,171,142,83,51,92,31,39,63');
+    assert.equal(actual.toString(), '4,133,182,171,142,83,51,92,31,39,63');
   });
 });
