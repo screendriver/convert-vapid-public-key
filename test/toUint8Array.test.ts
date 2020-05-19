17bf88610db0e86da7a9465404b7471c02c194e6
Christian Rackerseder <git@echooff.de>
2020-05-19 22:58:26 +0200

    💅 Format code with Prettier v2

--- test/toUint8Array.test.ts
+++ test/toUint8Array.test.ts
@@ -2,8 +2,8 @@ import { assert } from 'chai';
 import atob from 'atob';
 import { toUint8Array } from '../src/toUint8Array';
 
-suite('toUint8Array', function() {
-  test('convert given base64 string to an Uint8Array', function() {
+suite('toUint8Array', function () {
+  test('convert given base64 string to an Uint8Array', function () {
     const actual = toUint8Array('BIW2q45TM1wfJz9', atob);
     assert.equal(actual.toString(), '4,133,182,171,142,83,51,92,31,39,63');
   });
