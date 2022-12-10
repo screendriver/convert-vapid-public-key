cc2013cb2726e06567553a66ede82ae3f6db5e5d
Christian Rackerseder <git@echooff.de>
2022-12-10 09:21:31 +0100

    test: replace vitest with uvu

--- test/toUint8Array.test.ts
+++ test/toUint8Array.test.ts
@@ -1,9 +1,12 @@
-import { test, assert } from "vitest";
+import { test } from "uvu";
+import * as assert from "uvu/assert";
 import atob from "atob";
 import { toUint8Array } from "../src/toUint8Array.js";
 
 test("convert given base64 string to an Uint8Array", () => {
 	const actual = toUint8Array("BIW2q45TM1wfJz9", atob);
 
-	assert.strictEqual(actual.toString(), "4,133,182,171,142,83,51,92,31,39,63");
+	assert.equal(actual.toString(), "4,133,182,171,142,83,51,92,31,39,63");
 });
+
+test.run();
